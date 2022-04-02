7e3c517e668d47a283b3852331a6a48afd98154f
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-04-02 09:18:52 -0400

    rolling functionality

--- src/Dice.js
+++ src/Dice.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Dice(props) {
   return (
-    <span>
-      <i className={`Dice fa-solid fa-dice-${props.face}`}></i>
-    </span>
+    <i
+      className={`Dice fa-solid fa-dice-${props.face} ${
+        props.rolling && 'shaking'
+      }`}
+    ></i>
   );
 }
 
--- src/RollDice.js
+++ src/RollDice.js
@@ -4,7 +4,7 @@ import Dice from './Dice';
 class RollDice extends Component {
   constructor(props) {
     super(props);
-    this.state = { diceOne: 'one', diceTwo: 'one' };
+    this.state = { diceOne: 'one', diceTwo: 'one', isRolling: false };
     this.rollDice = this.rollDice.bind(this);
   }
   rollDice() {
@@ -24,15 +24,20 @@ class RollDice extends Component {
         : 'six';
     let val1 = toString(rand1);
     let val2 = toString(rand2);
-    this.setState({ diceOne: val1, diceTwo: val2 });
+    this.setState({ diceOne: val1, diceTwo: val2, isRolling: true });
+    setTimeout(() => {
+      this.setState({ isRolling: false });
+    }, 1000);
   }
   render() {
     return (
-      <div>
-        <Dice face={this.state.diceOne} />
-        <Dice face={this.state.diceTwo} />
+      <div className='RollDice'>
+        <Dice face={this.state.diceOne} rolling={this.state.isRolling} />
+        <Dice face={this.state.diceTwo} rolling={this.state.isRolling} />
         <br />
-        <button onClick={this.rollDice}>Roll Dice!</button>
+        <button onClick={this.rollDice} disabled={this.state.isRolling}>
+          {this.state.isRolling ? 'Rolling...' : 'Roll Dice'}
+        </button>
       </div>
     );
   }
